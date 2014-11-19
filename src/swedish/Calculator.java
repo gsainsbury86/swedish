e963305d72b739ad53f20ce63340bdd346e4137d
George Sainsbury <george.sainsbury@adelaide.edu.au>
2014-11-19 15:14:15 +1030

    added support for multiple simpleunitrules

--- src/swedish/Calculator.java
+++ src/swedish/Calculator.java
@@ -16,7 +16,7 @@ import java.util.HashMap;
 import org.json.*;
 
 public class Calculator {
-	
+
 	static boolean verbose = true;
 
 	public static void main(String[] args){
@@ -27,13 +27,17 @@ public class Calculator {
 			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
 			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
 			Document doc = dBuilder.parse(fXmlFile);
-			
+
 			ArrayList<Rule> rules = prepareRules();
 			
+			int totalCost = 0;
+
 			for(Rule rule : rules){
-				System.out.println(rule.calculate(doc));
-			}			
+				totalCost += rule.calculate(doc);
+			}
 			
+			System.out.println("Score: " + (300+(0.0+totalCost))/10.0);
+
 		}catch(Exception e){
 			e.printStackTrace();
 		}
@@ -41,42 +45,51 @@ public class Calculator {
 	}
 
 	private static ArrayList<Rule> prepareRules() throws IOException {
-		
-		
 
-			ArrayList<Rule> rules = new ArrayList<Rule>();
+
+
+		ArrayList<Rule> rules = new ArrayList<Rule>();
+
+
+
+		byte[] encoded = Files.readAllBytes(Paths.get("res/he_swedish.json"));
+		String jsonFileContents = new String(encoded, Charset.defaultCharset());
+
+		JSONObject obj = new JSONObject(jsonFileContents);
+
+		JSONArray simple = obj.getJSONArray("simple");
+
+		for (int j = 0; j < simple.length(); j++){
+			
+			JSONObject simpleObj = simple.getJSONObject(j);
+
+			String unitName = simpleObj.getString("name");
+
+			JSONArray arr = simpleObj.getJSONArray("costs");
 			
 			HashMap<Range,String> rangesToPoints = new HashMap<Range,String>();
-			
-			byte[] encoded = Files.readAllBytes(Paths.get("res/he_swedish.json"));
-			String jsonFileContents = new String(encoded, Charset.defaultCharset());
-			
-			JSONObject obj = new JSONObject(jsonFileContents);
-			
-			
-			String unitName = obj.getString("name");
-			
-			JSONArray arr = obj.getJSONArray("costs");
 
 			for (int i = 0; i < arr.length(); i++)
 			{
-			    int rangeLow = arr.getJSONObject(i).getInt("low");
-			    int rangeHigh = arr.getJSONObject(i).getInt("high");
-					
-			    String cost = arr.getJSONObject(i).getString("cost");
-			    
-			    rangesToPoints.put(new Range(rangeLow,rangeHigh), cost);
-			    
-			    //System.out.printf("%d %d %s\n",rangeLow,rangeHigh,cost);
+				int rangeLow = arr.getJSONObject(i).getInt("low");
+				int rangeHigh = arr.getJSONObject(i).getInt("high");
+
+				String cost = arr.getJSONObject(i).getString("cost");
+
+				rangesToPoints.put(new Range(rangeLow,rangeHigh), cost);
+
+				//System.out.printf("%d %d %s\n",rangeLow,rangeHigh,cost);
 			}
-			
+
 			Rule r = new SimpleUnitRule(unitName,rangesToPoints);
-			
+
 			rules.add(r);
-			
-			return rules;
-		
-		
+
+		}
+
+		return rules;
+
+
 	}
 
 }
