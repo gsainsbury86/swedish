3267eb8ef688d3d657b2394933698c816aad8ff7
George Sainsbury <george.sainsbury@adelaide.edu.au>
2014-11-19 18:25:02 +1030

    added support for scalable duplicates and simple single models

--- src/swedish/Calculator.java
+++ src/swedish/Calculator.java
@@ -29,13 +29,13 @@ public class Calculator {
 			Document doc = dBuilder.parse(fXmlFile);
 
 			ArrayList<Rule> rules = prepareRules();
-			
+
 			int totalCost = 0;
 
 			for(Rule rule : rules){
 				totalCost += rule.calculate(doc);
 			}
-			
+
 			System.out.println("Score: " + (300+(0.0+totalCost))/10.0);
 
 		}catch(Exception e){
@@ -47,27 +47,38 @@ public class Calculator {
 	// http://www.jsoneditoronline.org
 	private static ArrayList<Rule> prepareRules() throws IOException {
 
-
-
 		ArrayList<Rule> rules = new ArrayList<Rule>();
-
-
 
 		byte[] encoded = Files.readAllBytes(Paths.get("res/he_swedish.json"));
 		String jsonFileContents = new String(encoded, Charset.defaultCharset());
 
 		JSONObject obj = new JSONObject(jsonFileContents);
 
+		rules.addAll(prepareSimpleUnitRules(obj));
+
+		rules.addAll(prepareDuplicateRules(obj));
+
+		rules.addAll(prepareSimpleSingleModelRules(obj));
+
+		return rules;
+
+	}
+
+	public static ArrayList<Rule> prepareSimpleUnitRules(JSONObject obj){
+
+
+		ArrayList<Rule> rules = new ArrayList<Rule>();
+
 		JSONArray simple = obj.getJSONArray("simple");
 
 		for (int j = 0; j < simple.length(); j++){
-			
+
 			JSONObject simpleObj = simple.getJSONObject(j);
 
 			String unitName = simpleObj.getString("name");
 
 			JSONArray arr = simpleObj.getJSONArray("costs");
-			
+
 			HashMap<Range,String> rangesToPoints = new HashMap<Range,String>();
 
 			for (int i = 0; i < arr.length(); i++)
@@ -89,8 +100,73 @@ public class Calculator {
 		}
 
 		return rules;
+	}
+
+
+
+	public static ArrayList<Rule> prepareDuplicateRules(JSONObject obj){
+
+
+		ArrayList<Rule> rules = new ArrayList<Rule>();
+
+
+		JSONArray duplicate = obj.getJSONArray("duplicate");
+
+		for (int j = 0; j < duplicate.length(); j++){
+
+			JSONObject duplicateObj = duplicate.getJSONObject(j);
+
+			String unitName = duplicateObj.getString("name");
+
+			JSONArray arr = duplicateObj.getJSONArray("costs");
+
+			HashMap<Range,String> rangesToPoints = new HashMap<Range,String>();
+
+			for (int i = 0; i < arr.length(); i++)
+			{
+				int rangeLow = arr.getJSONObject(i).getInt("low");
+				int rangeHigh = arr.getJSONObject(i).getInt("high");
+
+				String cost = arr.getJSONObject(i).getString("cost");
+
+				rangesToPoints.put(new Range(rangeLow,rangeHigh), cost);
+
+				//System.out.printf("%d %d %s\n",rangeLow,rangeHigh,cost);
+			}
+
+			Rule r = new DuplicateRule(unitName,rangesToPoints);
+
+			rules.add(r);
+
+		}
+
+		return rules;
+	}
+
+
+	public static ArrayList<Rule> prepareSimpleSingleModelRules(JSONObject obj){
+
+
+		ArrayList<Rule> rules = new ArrayList<Rule>();
+
+		JSONArray simpleModel = obj.getJSONArray("simple_model");
+
+		for (int j = 0; j < simpleModel.length(); j++){
+
+			JSONObject simpleModeleObj = simpleModel.getJSONObject(j);
+
+			String unitName = simpleModeleObj.getString("name");
+
+			int cost = simpleModeleObj.getInt("cost");
+
+			Rule r = new SimpleSingleModelRule(unitName,cost);
+
+			rules.add(r);
+
+		}
+
+		return rules;
 
 
 	}
-
 }
